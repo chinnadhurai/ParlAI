8fb02e6e87c8365b0d70121f8caf80e7efe340c0
chinnadhurai <chinnadhurai@gmail.com>
2019-02-23 15:37:24 -0500

    add no_perturb commmand

--- run_perturb.sh
+++ run_perturb.sh
@@ -4,22 +4,27 @@
 LOGDIR="perturb_log_files/"
 mkdir -p $LOGDIR
 
-for DATASET in "personachat" "dailydialog"
+for DATASET in "dailydialog" "personachat"
 do
     for MODEL_TYPE in "seq2seq" #"seq2seq_attn" "transfomers"  for DATATYPE in "valid" "test"
     do
-        for PERTURB_TYPE in "swap" "repeat" "drop"
+        for DATATYPE in "test" #valid
         do
-            for PERTURB_LOC in "first" "last" "random"
+            echo "---------------------"
+            echo "CONFIG : "$DATASET"_"$MODEL_TYPE"_"$DATATYPE"_NoPerturb"
+            LOGFILE=$LOGDIR/log_$DATASET"_"$MODEL_TYPE"_"$DATATYPE"_no_perturb.txt"
+            python -W ignore examples/eval_model.py -m $MODEL_TYPE -t $DATASET -mf "save_dir/model_"$DATASET"_s2s" -sft True -pb "None" --datatype $DATATYPE > $LOGFILE
+            grep FINAL_REPORT $LOGFILE
+            
+            for PERTURB_TYPE in "swap" "repeat" "drop"
             do
-                for DATATYPE in "valid" "test"
+                for PERTURB_LOC in "first" "last" "random"
                 do
-                    LOGFILE=$LOGDIR/log_$DATASET"_"$MODEL_TYPE"_"$DATATYPE"_"$PERTURB_TYPE$PERTURB_LOC".txt"
-                    python -W ignore examples/eval_model.py -m $MODEL_TYPE -t $DATASET -mf "save_dir/model_"$DATASET"_s2s" -sft True -pb $PERTURB_TYPE_$PERTURB_LOC --datatype $DATATYPE > $LOGFILE
                     echo "---------------------"
-                    echo "CONFIG : "$DATASET"_"$MODEL_TYPE"_"$DATATYPE"_"$PERTURB_TYPE$PERTURB_LOC
+                    echo "CONFIG : "$DATASET"_"$MODEL_TYPE"_"$DATATYPE"_"$PERTURB_TYPE"_"$PERTURB_LOC
+                    LOGFILE=$LOGDIR/log_$DATASET"_"$MODEL_TYPE"_"$DATATYPE"_"$PERTURB_TYPE"_"$PERTURB_LOC".txt"
+                    python -W ignore examples/eval_model.py -m $MODEL_TYPE -t $DATASET -mf "save_dir/model_"$DATASET"_s2s" -sft True -pb $PERTURB_TYPE"_"$PERTURB_LOC --datatype $DATATYPE > $LOGFILE
                     grep FINAL_REPORT $LOGFILE
-                    echo "---------------------"
                 done
             done
         done 
