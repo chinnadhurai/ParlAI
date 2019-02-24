cbf81e99cf8e911d8ec645c6bf11f451b0e366c8
MaximumEntropy <sandeep.subramanian@maluuba.com>
2019-02-24 02:38:55 -0500

    fix issue in resolving merge conflict

--- run.sh
+++ run.sh
@@ -21,13 +21,15 @@ GPU_ARGS=" --gpu "$GPU
 if [ "$2" = "s2s" ]
 then
     echo "MODELTYPE: "$2
-    EVAL_MODEL_ARGS="-m seq2seq"
-    TRAIN_MODEL_ARGS=$EVAL_MODEL_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000 --bidirectional true"
+    COMMON_ARGS=$GPU_ARGS" -m seq2seq"
+    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 1"
+    TRAIN_MODEL_ARGS=$COMMON_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000 --bidirectional true"
 elif [ "$2" = "s2s_att_general" ]
 then
     echo "MODELTYPE: "$2
-    EVAL_MODEL_ARGS="-m seq2seq -att general"
-    TRAIN_MODEL_ARGS=$EVAL_MODEL_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000 --bidirectional true"
+    COMMON_ARGS=$GPU_ARGS" -m seq2seq -att general"
+    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 1"
+    TRAIN_MODEL_ARGS=$COMMON_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000 --bidirectional true"
 elif [ "$2" = "transformer" ]
 then
     echo "MODELTYPE: "$2
