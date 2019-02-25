fcb2ce7a9f4f267fe843b6034bf87bbc022a637f
chinnadhurai <chinnadhurai@gmail.com>
2019-02-24 22:44:39 -0500

    eval batchsize to 64

--- run.sh
+++ run.sh
@@ -22,32 +22,32 @@ if [ "$2" = "s2s" ]
 then
     echo "MODELTYPE: "$2
     COMMON_ARGS=$GPU_ARGS" -m seq2seq"
-    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 1 -d True"
+    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 64 -d True"
     TRAIN_MODEL_ARGS=$COMMON_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000"
 elif [ "$2" = "s2s_bidir" ]
 then
     echo "MODELTYPE: "$2
     COMMON_ARGS=$GPU_ARGS" -m seq2seq --bidirectional true --hiddensize 256"
-    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 1 -d True"
+    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 64 -d True"
     TRAIN_MODEL_ARGS=$COMMON_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000"
 elif [ "$2" = "s2s_att_general" ]
 then
     echo "MODELTYPE: "$2
     COMMON_ARGS=$GPU_ARGS" -m seq2seq -att general"
-    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 1 -d True"
+    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 64 -d True"
     TRAIN_MODEL_ARGS=$COMMON_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000"
 elif [ "$2" = "s2s_att_general_bidir" ]
 then
     echo "MODELTYPE: "$2
     COMMON_ARGS=$GPU_ARGS" -m seq2seq -att general --bidirectional true --hiddensize 256"
-    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 1 -d True"
+    EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 64 -d True"
     TRAIN_MODEL_ARGS=$COMMON_ARGS" -vmt loss -eps 60 -veps 1 -stim 600 -bs 32 --optimizer adam --lr-scheduler invsqrt -lr 0.005 --dropout 0.3 --warmup-updates 4000"
 elif [ "$2" = "transformer" ]
 then
     echo "MODELTYPE: "$2
-    COMMON_ARGS=$GPU_ARGS" -m transformer/generator -bs 64"
+    COMMON_ARGS=$GPU_ARGS" -m transformer/generator"
     EVAL_MODEL_ARGS=$COMMON_ARGS" -bs 64 -d True" 
-    TRAIN_MODEL_ARGS=$COMMON_ARGS" --optimizer adam -lr 0.001 --lr-scheduler invsqrt --warmup-updates 4000 -eps 25 -veps 1 -stim 600" 
+    TRAIN_MODEL_ARGS=$COMMON_ARGS" -bs 64 --optimizer adam -lr 0.001 --lr-scheduler invsqrt --warmup-updates 4000 -eps 25 -veps 1 -stim 200" 
 else
     echo "INVALID modeltype : "$2" Supported : s2s, s2s_att_general, transformer"
     echo "Example train command : sh run.sh train <model_type> <dataset>"
